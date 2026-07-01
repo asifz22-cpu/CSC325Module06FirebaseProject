0152a629ef5fea4229788c76f373e09f54392887
zeshanasif <asifz22@farmingdale.edu>
2026-07-01 18:24:45 -0400

    Final module 06 project

--- src/main/java/com/example/csc325_firebase_webview_auth/view/AccessFBView.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/AccessFBView.java
@@ -1,7 +1,6 @@
-package com.example.csc325_firebase_webview_auth.view;//package modelview;
+package com.example.csc325_firebase_webview_auth.view;
 
 import com.example.csc325_firebase_webview_auth.model.Person;
-import com.example.csc325_firebase_webview_auth.viewmodel.AccessDataViewModel;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
@@ -10,10 +9,10 @@ import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.UserRecord;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -21,38 +20,49 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+
+import java.io.File;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 public class AccessFBView {
 
-
-     @FXML
+    @FXML
     private TextField nameField;
+
     @FXML
     private TextField majorField;
+
     @FXML
     private TextField ageField;
+
     @FXML
-    private Button writeButton;
+    private TableView<Person> tableView;
+
     @FXML
-    private Button readButton;
+    private TableColumn<Person, String> nameColumn;
+
     @FXML
-    private TextArea outputField;
-     private boolean key;
+    private TableColumn<Person, String> majorColumn;
+
+    @FXML
+    private TableColumn<Person, Integer> ageColumn;
+    @FXML
+    private ImageView profileImage;
+
     private ObservableList<Person> listOfUsers = FXCollections.observableArrayList();
-    private Person person;
-    public ObservableList<Person> getListOfUsers() {
-        return listOfUsers;
-    }
 
-    void initialize() {
+    @FXML
+    public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        majorColumn.setCellValueFactory(new PropertyValueFactory<>("major"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<>("age"));
 
-        AccessDataViewModel accessDataViewModel = new AccessDataViewModel();
-        nameField.textProperty().bindBidirectional(accessDataViewModel.userNameProperty());
-        majorField.textProperty().bindBidirectional(accessDataViewModel.userMajorProperty());
-        writeButton.disableProperty().bind(accessDataViewModel.isWritePossibleProperty().not());
+        tableView.setItems(listOfUsers);
     }
 
     @FXML
@@ -60,101 +70,214 @@ public class AccessFBView {
         addData();
     }
 
-        @FXML
+    @FXML
     private void readRecord(ActionEvent event) {
         readFirebase();
     }
 
-            @FXML
-    private void regRecord(ActionEvent event) {
-        registerUser();
+    @FXML
+    private void clearRecord(ActionEvent event) {
+        clearBoxes();
     }
 
-     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("/files/WebContainer.fxml");
+    @FXML
+    private void regRecord(ActionEvent event) {
+        showRegisterForm();
+    }
+
+    @FXML
+    private void signInRecord(ActionEvent event) {
+        showSignInForm();
     }
 
     public void addData() {
-
         DocumentReference docRef = App.fstore.collection("References").document(UUID.randomUUID().toString());
 
         Map<String, Object> data = new HashMap<>();
         data.put("Name", nameField.getText());
         data.put("Major", majorField.getText());
         data.put("Age", Integer.parseInt(ageField.getText()));
-        //asynchronously write data
+
         ApiFuture<WriteResult> result = docRef.set(data);
+
+        Person person = new Person(
+                nameField.getText(),
+                majorField.getText(),
+                Integer.parseInt(ageField.getText())
+        );
+
+        listOfUsers.add(person);
+        clearBoxes();
     }
 
-        public boolean readFirebase()
-         {
-             key = false;
+    public boolean readFirebase() {
+        listOfUsers.clear();
 
-        //asynchronously retrieve all documents
-        ApiFuture<QuerySnapshot> future =  App.fstore.collection("References").get();
-        // future.get() blocks on response
-        List<QueryDocumentSnapshot> documents;
-        try
-        {
-            documents = future.get().getDocuments();
-            if(documents.size()>0)
-            {
-                System.out.println("Outing....");
-                for (QueryDocumentSnapshot document : documents)
-                {
-                    outputField.setText(outputField.getText()+ document.getData().get("Name")+ " , Major: "+
-                            document.getData().get("Major")+ " , Age: "+
-                            document.getData().get("Age")+ " \n ");
-                    System.out.println(document.getId() + " => " + document.getData().get("Name"));
-                    person  = new Person(String.valueOf(document.getData().get("Name")),
-                            document.getData().get("Major").toString(),
-                            Integer.parseInt(document.getData().get("Age").toString()));
-                    listOfUsers.add(person);
-                }
-            }
-            else
-            {
-               System.out.println("No data");
-            }
-            key=true;
+        ApiFuture<QuerySnapshot> future = App.fstore.collection("References").get();
 
-        }
-        catch (InterruptedException | ExecutionException ex)
-        {
-             ex.printStackTrace();
-        }
-        return key;
-    }
-
-        public void sendVerificationEmail() {
         try {
-            UserRecord user = App.fauth.getUser("name");
-            //String url = user.getPassword();
+            List<QueryDocumentSnapshot> documents = future.get().getDocuments();
 
-        } catch (Exception e) {
+            for (QueryDocumentSnapshot document : documents) {
+                String name = String.valueOf(document.getData().get("Name"));
+                String major = String.valueOf(document.getData().get("Major"));
+                int age = Integer.parseInt(String.valueOf(document.getData().get("Age")));
+
+                Person person = new Person(name, major, age);
+                listOfUsers.add(person);
+            }
+
+            return true;
+
+        } catch (InterruptedException | ExecutionException ex) {
+            ex.printStackTrace();
+            return false;
         }
     }
 
-    public boolean registerUser() {
+    public void clearBoxes() {
+        nameField.clear();
+        majorField.clear();
+        ageField.clear();
+    }
+
+    public void showRegisterForm() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Register User");
+
+        TextField emailBox = new TextField();
+        emailBox.setPromptText("Email");
+
+        PasswordField passwordBox = new PasswordField();
+        passwordBox.setPromptText("Password");
+
+        TextField nameBox = new TextField();
+        nameBox.setPromptText("Name");
+
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+
+        grid.add(new Label("Email:"), 0, 0);
+        grid.add(emailBox, 1, 0);
+
+        grid.add(new Label("Password:"), 0, 1);
+        grid.add(passwordBox, 1, 1);
+
+        grid.add(new Label("Name:"), 0, 2);
+        grid.add(nameBox, 1, 2);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+        Optional<ButtonType> answer = dialog.showAndWait();
+
+        if (answer.isPresent() && answer.get() == ButtonType.OK) {
+            registerUser(emailBox.getText(), passwordBox.getText(), nameBox.getText());
+        }
+    }
+
+    public void showSignInForm() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Sign In");
+
+        TextField emailBox = new TextField();
+        emailBox.setPromptText("Email");
+
+        PasswordField passwordBox = new PasswordField();
+        passwordBox.setPromptText("Password");
+
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(10);
+
+        grid.add(new Label("Email:"), 0, 0);
+        grid.add(emailBox, 1, 0);
+
+        grid.add(new Label("Password:"), 0, 1);
+        grid.add(passwordBox, 1, 1);
+
+        dialog.getDialogPane().setContent(grid);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+        Optional<ButtonType> answer = dialog.showAndWait();
+
+        if (answer.isPresent() && answer.get() == ButtonType.OK) {
+            signInUser(emailBox.getText());
+        }
+    }
+
+    public boolean registerUser(String email, String password, String name) {
         UserRecord.CreateRequest request = new UserRecord.CreateRequest()
-                .setEmail("user@example.com")
-                .setEmailVerified(false)
-                .setPassword("secretPassword")
-                .setPhoneNumber("+11234567890")
-                .setDisplayName("John Doe")
+                .setEmail(email)
+                .setPassword(password)
+                .setDisplayName(name)
                 .setDisabled(false);
 
-        UserRecord userRecord;
         try {
-            userRecord = App.fauth.createUser(request);
-            System.out.println("Successfully created new user: " + userRecord.getUid());
+            UserRecord userRecord = App.fauth.createUser(request);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Registered");
+            alert.setHeaderText(null);
+            alert.setContentText("User created: " + userRecord.getEmail());
+            alert.show();
+
             return true;
 
         } catch (FirebaseAuthException ex) {
-           // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Register Error");
+            alert.setHeaderText(null);
+            alert.setContentText("User was not created.");
+            alert.show();
+
             return false;
         }
+    }
 
+    public boolean signInUser(String email) {
+        try {
+            UserRecord userRecord = App.fauth.getUserByEmail(email);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Sign In");
+            alert.setHeaderText(null);
+            alert.setContentText("Signed in as: " + userRecord.getEmail());
+            alert.show();
+
+            return true;
+
+        } catch (FirebaseAuthException ex) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Sign In Error");
+            alert.setHeaderText(null);
+            alert.setContentText("User was not found.");
+            alert.show();
+
+            return false;
+        }
+    }
+    @FXML
+    private void choosePicture(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose Profile Picture");
+
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg")
+        );
+
+        File file = fileChooser.showOpenDialog(profileImage.getScene().getWindow());
+
+        if (file != null) {
+            Image image = new Image(file.toURI().toString());
+            profileImage.setImage(image);
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Picture Selected");
+            alert.setHeaderText(null);
+            alert.setContentText("Picture selected. Firebase Storage needs billing, so this shows it locally.");
+            alert.show();
+        }
     }
 }
--- src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/view/App.java
@@ -1,33 +1,67 @@
 package com.example.csc325_firebase_webview_auth.view;
 
-
 import com.example.csc325_firebase_webview_auth.model.FirestoreContext;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.auth.FirebaseAuth;
 import java.io.IOException;
+
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     public static Firestore fstore;
     public static FirebaseAuth fauth;
     public static Scene scene;
+
     private final FirestoreContext contxtFirebase = new FirestoreContext();
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
+
         fstore = contxtFirebase.firebase();
         fauth = FirebaseAuth.getInstance();
-        scene = new Scene(loadFXML("/files/AccessFBView.fxml"));
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+        VBox splash = new VBox(15);
+        splash.setAlignment(Pos.CENTER);
+        splash.setStyle("-fx-background-color: #bf7f2d;");
+
+        Label title = new Label("CSC325 Firebase Project");
+        title.setStyle("-fx-font-size: 28px; -fx-text-fill: white;");
+
+        Label loading = new Label("Loading...");
+        loading.setStyle("-fx-font-size: 18px; -fx-text-fill: white;");
+
+        ProgressIndicator circle = new ProgressIndicator();
+
+        splash.getChildren().addAll(title, circle, loading);
+
+        scene = new Scene(splash, 900, 550);
+
+        stage.setTitle("CSC325 Module 06 Project");
+        stage.setScene(scene);
+        stage.show();
+
+        PauseTransition pause = new PauseTransition(Duration.seconds(2));
+
+        pause.setOnFinished(e -> {
+            try {
+                scene.setRoot(loadFXML("/files/AccessFBView.fxml"));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+
+        pause.play();
     }
 
     public static void setRoot(String fxml) throws IOException {
@@ -35,12 +69,11 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml ));
-        return fxmlLoader.load();
+        FXMLLoader loader = new FXMLLoader(App.class.getResource(fxml));
+        return loader.load();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
